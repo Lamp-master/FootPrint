0a653accff174970d54cfaccb26c2aa62c458ba0
HBG <kjil150@nate.com>
2020-09-26 13:24:47 +0100

    Create LoginActivity, MainActivity, login.xml

---
+++ app/src/main/java/com/gachon/footprint/LoginActivity.kt
@@ -0,0 +1,208 @@
+package com.gachon.footprint
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+import timber.log.Timber
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+class LoginActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private var googleSignInClient: GoogleSignInClient? = null
+    private var GOOGLE_LOGIN_CODE = 9001
+    private var callbackManager: CallbackManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        Timber.plant(Timber.DebugTree())
+
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+        google_sign_in_button.setOnClickListener {
+            //구글 첫번째
+            googleLogin()
+        }
+        facebook_login_button.setOnClickListener {
+            //페이스북 첫번째
+            facebookLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("425957488645-nea69jaa49iv007nkcv4qs7co6pmv3ue.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // printHashKey()
+        callbackManager = CallbackManager.Factory.create()
+    } // 페이스북 Hash 값 : ZNHQWY2e5GfJJWjgerEPCatjaTI=
+
+    //region facebook hashkey
+    fun printHashKey() {
+        try {
+            val info = packageManager.getPackageInfo(
+                packageName,
+                PackageManager.GET_SIGNING_CERTIFICATES
+            )
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
+        }
+    }
+    //endregion
+
+    //region google login method
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+    //endregion
+
+    //region facebook Login
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    // 페이스북 2번째
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+    }
+    //endregion
+
+    //region facebook token handler
+    fun handleFacebookAccessToken(token: AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //페이스북 3번쨰
+                    //login
+                    moveMainPage(task.result?.user)
+                } else {
+                    //에러 메세지
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
+    //endregion
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result?.isSuccess!!) {
+                var account = result?.signInAccount
+                // 두번째
+                firebaseAuthWithGoogle(account)
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        // account 안에 있는 토큰 아이디 넘김
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //login
+                    moveMainPage(task.result?.user)
+                } else {
+                    //에러 메세지
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+
+                }
+            }
+    }
+
+    //region email login method
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            email_edittext.text.toString(),
+            password_edittext.text.toString()
+        )?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // 새 계정 만듬
+                moveMainPage(task.result?.user)
+            } else if (task.exception?.message.isNullOrEmpty()) {
+                // 에러 메세지 출력
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            } else {
+                signinEmail()
+                // login if you have account
+            }
+        }
+    }
+    //endregion
+
+    //region emaillogin
+    private fun signinEmail() {
+        auth?.createUserWithEmailAndPassword(
+            email_edittext.text.toString(),
+            password_edittext.text.toString()
+        )?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                //login
+                moveMainPage(task.result?.user)
+            } else {
+                //에러 메세지
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    //endregion
+
+    private fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
