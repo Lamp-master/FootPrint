7bd472dd223908cb4b914e5c3583970735e2a385
HBG <kjil150@nate.com>
2020-10-04 11:04:16 +0100

    Add google map and currentlocation marker print

--- app/src/main/java/com/gachon/footprint/MainActivity.kt
+++ app/src/main/java/com/gachon/footprint/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.gachon.footprint
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.gachon.footprint.navigation.CashFragment
 import com.gachon.footprint.navigation.HomeFragment
 import com.gachon.footprint.navigation.SettingFragment
 import com.gachon.footprint.navigation.UserFragment
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    companion object {
+        var sydney: LatLng? = null
+    }
+
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
         when (p0.itemId) {
             R.id.action_home -> {
@@ -46,12 +58,50 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        Timber.plant(Timber.DebugTree())
 
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
         bottom_navigation.selectedItemId=R.id.action_home
+
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions,0)
+        }
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(this, permissions, 0)
 
         google_map_start.setOnClickListener {
             startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        fun getLastLocationNewMethod() {
+            val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            mFusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    location?.apply {
+                        sydney = LatLng(this.latitude, this.longitude)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    e.printStackTrace()
+                }
         }
         /*ar_test.setOnClickListener {
             startActivity(Intent(this, CameraActivity::class.java))*/
--- app/src/main/java/com/gachon/footprint/MapActivity.kt
+++ app/src/main/java/com/gachon/footprint/MapActivity.kt
@@ -1,19 +1,29 @@
 package com.gachon.footprint
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var  mMap: GoogleMap
+    private var mMap: GoogleMap? = null
+    var lat: Double? = 0.0
+    var lon: Double? = 0.0
+
+    var currentLocation: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +34,49 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val marker= LatLng(35.241615,128.695587)
-        mMap.addMarker(MarkerOptions().position(marker).title("Marker LAB"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        getLastLocationNewMethod()
     }
+
+    fun getLastLocationNewMethod() {
+        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                location?.apply {
+                    val sydney = LatLng(this.latitude, this.longitude)
+                    lat = latitude
+                    lon = longitude
+                    getCurrentLoc()
+                    mMap?.addMarker(MarkerOptions().position(sydney).title(currentLocation))
+                    mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                    mMap?.animateCamera(CameraUpdateFactory.zoomTo(16f))
+
+                }
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+            }
+    }
+
+    fun getCurrentLoc() {
+        val mGeoCoder = Geocoder(applicationContext, Locale.KOREAN)
+        var mResultList: List<Address>? = null
+        try {
+            mResultList = mGeoCoder.getFromLocation(lat!!, lon!!, 1)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        if (mResultList != null) {
+            currentLocation = mResultList[0].getAddressLine(0)
+            currentLocation = currentLocation.substring(11)
+        }
+    }
+
 }
+
+
