3f987eb8b4a1c10670eb8cc69c32caa8d6d2e93e
Silverhook <cupddongdl@naver.com>
2020-10-11 14:34:47 +0900

    백업용/이미지 할것 1. Firestore Storage에 업로드/ 2.버튼 이동 추가/ 3. 사용자 footlist에 foot저장하기

---
+++ app/src/main/java/com/gachon/footprint/FootMsgActivity.kt
@@ -0,0 +1,237 @@
+package com.gachon.footprint
+
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.opengl.Visibility
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.gachon.footprint.data.ModelFoot
+import com.gachon.footprint.data.ModelUser
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_footprint.*
+import kotlinx.android.synthetic.main.activity_login.*
+import java.io.FileOutputStream
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
+
+// 제목/사진/내용/위치정보를 받아서 firestore에 등록하는 액티비티
+class FootMsgActivity : AppCompatActivity() {
+    var user = FirebaseAuth.getInstance().currentUser
+    val db= FirebaseFirestore.getInstance()
+    var footmsgInfo = ModelFoot()
+
+    val footMsgRef = db.collection("FootMsg")
+
+    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+    //저장소 R/W을 받는 권한설정()
+    val STORAGE_PERMISSION = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE
+        , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val FLAG_PERM_CAMERA = 98
+    val FLAG_PERM_STORAGE = 99
+
+    val FLAG_REQ_CAMERA = 101
+    val FLAG_REQ_STORAGE = 102
+
+
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_footprint)
+
+        //제목 : add_footprint_title -Title, 사진 : footprintImg - FootMedia,
+        // add_footprint_location - Location : 위치정보, 내용 : add_footprint_context
+        //내장실행->남긴시간(Timestamp-WrittenTime),
+
+        //전 액티비티에서 위치정보 받고 이를 위도 경도로 표시해 주기
+
+
+        //권한을 받고 이미지뷰(footPrintImg)클릭해서 이미지 저장소에서 가져오기
+        if (checkPermission(STORAGE_PERMISSION, FLAG_PERM_STORAGE)) {
+            setViews()
+        }
+        //확인버튼시 firestore createFootMsg 및 footlist 등록
+        confirm_button.setOnClickListener{
+            footmsgInfo.title= add_footprint_title.text.toString()
+            footmsgInfo.msgText=add_footprint_context.text.toString()
+
+
+        }
+
+    }
+
+
+
+
+    //이미지 저장소에서 불러오기
+    private fun setViews(){
+        footprintImg.setOnClickListener {
+            openCamera()
+        }
+        footprintImg.setOnClickListener {
+            openGallery()
+        }
+    }
+    //카메라로 찍기
+    fun openCamera() {
+        if (checkPermission(CAMERA_PERMISSION, FLAG_PERM_CAMERA)) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            ActivityCompat.startActivityForResult(intent, FLAG_REQ_CAMERA)
+        }
+    }
+    //개인 저장소 개인 갤러리 열기
+    fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        ActivityCompat.startActivityForResult(intent, FLAG_REQ_STORAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK) {
+            when(requestCode){
+                FLAG_REQ_CAMERA -> {
+                    if (data?.extras?.get("data") != null) {
+                        val bitmap = data?.extras?.get("data") as Bitmap
+//                        imagePreview.setImageBitmap(bitmap)
+                        val uri = saveImageFile(newFileName(), "image/jpg", bitmap)
+                        footprintImg.setImageURI(uri)
+                    }
+                }
+                FLAG_REQ_STORAGE -> {
+                    val uri = data?.data
+                    footprintImg.setImageURI(uri)
+                }
+            }
+        }
+    }
+
+    private fun saveImageFile(filename:String, mimeType:String, bitmap: Bitmap) : Uri? {
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        try {
+            if(uri != null) {
+                var descriptor = contentResolver.openFileDescriptor(uri, "w")
+                if(descriptor != null) {
+                    val fos = FileOutputStream(descriptor.fileDescriptor)
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                    fos.close()
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        values.clear()
+                        values.put(MediaStore.Images.Media.IS_PENDING, 0)
+                        contentResolver.update(uri, values, null, null)
+                    }
+                }
+            }
+        }catch (e:java.lang.Exception) {
+            Log.e("File", "error=${e.localizedMessage}")
+        }
+        return uri
+    }
+
+    private fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+
+        return "$filename.jpg"
+    }
+
+    /*
+    * 여기서 부터 권한처리 관련 함수
+    */
+    private fun checkPermission(permissions: Array<out String>, flag:Int) : Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, permissions, flag)
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int
+                                            , permissions: Array<out String>
+                                            , grantResults: IntArray) {
+        when (requestCode) {
+            FLAG_PERM_STORAGE -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "저장소 권한을 승인해야지만 앱을 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+                        finish()
+                        return
+                    }
+                }
+                setViews()
+            }
+            FLAG_PERM_CAMERA -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "카메라 권한을 승인해야지만 카메라를 사용할 수 있습니다.", Toast.LENGTH_LONG).show()
+                        return
+                    }
+                }
+                openCamera()
+            }
+        }
+    }
+    private fun getCurrentTime(){
+        val timestamp = it["time"] as com.google.firebase.Timestamp
+        val milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        val netDate = Date(milliseconds)
+        val date = sdf.format(netDate).toString()
+        Log.d("TAG170", date)
+    }
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/gachon/footprint/data/ModelFoot.kt
@@ -0,0 +1,33 @@
+package com.gachon.footprint.data
+
+import android.location.Location
+import android.provider.MediaStore
+
+data class ModelFoot(
+    var title: String? =null,
+    var uid:String?=null,
+    var nickname:String?=null,
+    var timestamp:Long?=null,
+    var msgText: String?=null,
+    var msgImg: MediaStore.Audio.Media?=null,
+    var msgMedia: MediaStore.Video.Media? =null,
+    var location: Location?=null,//받는 거 어케 받지
+    var arMarker: String?=null){
+
+    data class Review(var uid:String?= null,
+                      var nickname: String?=null,
+                      var msgText: String?=null,
+                      var recommend : Boolean?=null,
+                      var report : Boolean? = null)
+
+}
+
+
+
+
+
+
+
+
+
+
